e32ed1a90c24500257d46e7a058912a35cacc9a9
rajyadav <vector36razz@gmail.com>
2025-02-05 23:10:27 +0530

    express code added

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 // const express = require('express') //old way, not working nowsdays
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = 3030;
+
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
+
+
+
 
 // app.get("/", (req, res) => {
 //     res.send("Hello from Razz");
